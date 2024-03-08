e13fb8b250526fa0d2c65a5ac8645cc75ec8ccd0
UdaykiranReddy-1 <118445671+UdaykiranReddy-1@users.noreply.github.com>
2024-03-08 17:03:40 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "I have successfully added WebHook for\n";
+  cout << "Testing for pipeline\n";
   return 0;
 }
